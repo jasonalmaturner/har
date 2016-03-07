ed880876f3fc4e101fe95c5d8f059cbddde00429
Jason Turner <jasonalmaturner@gmail.com>
2016-03-07 13:27:29 -0700

    taking a break to go on a run

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import styles from './App.css';
+import configureStore from '../../store/configureStore';
+
+const store = configureStore();
 
 const App = () => (
-  <div>
-    test
-  </div>
+  <Provider store={store}>
+    <div>
+      this is a test
+    </div>
+  </Provider>
 );
 
 export default App;
