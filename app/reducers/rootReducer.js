fb20046977efe20a19e5edb82dd6814fef59e207
Jason Turner <jasonalmaturner@gmail.com>
2016-03-07 13:20:37 -0700

    combined reducers

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { uploadFiles } from './harUploadReducer';
+import { displayData } from './harDataReducer';
 
 const rootReducer = combineReducers({
-
+  uploadFiles,
+  displayData,
 });
 
 export default rootReducer;
