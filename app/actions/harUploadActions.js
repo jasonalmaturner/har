1042ef64106f543c6d71ed00170e8c3361886139
Jason Turner <jasonalmaturner@gmail.com>
2016-03-09 11:27:21 -0700

    cleaning up

--- app/actions/harUploadActions.js
+++ app/actions/harUploadActions.js
@@ -44,29 +44,11 @@ export function sendFile() {
       }));
     }
 
-    // let data = new FormData();
-    // console.log(1111, data);
-    // appendFiles(data, harsA, 'aFiles');
-    // appendFiles(data, harsB, 'bFiles');
-    // for (var pair of data) {
-    //   console.log(pair[0], pair[1], pair)
-    // }
     const options = {
       headers: {
         'Content-Type': 'application/json',
       },
     };
-
-    // axios.post('/api/files', {
-    //   harsA,
-    //   harsB,
-    // }, options).then(res => {
-    //   dispatch(receiveHarData(res.data.a, res.data.b));
-    //   return dispatch(receiveUploadResponse());
-    // }).catch(err => {
-    //   console.log('upload err', err);
-    //   return dispatch(handleUploadError(err));
-    // });
 
     let harsAFiles = readFiles(harsA);
     let harsBFiles = readFiles(harsB);
@@ -83,14 +65,6 @@ export function sendFile() {
       return dispatch(handleUploadError(err));
     });
   };
-}
-
-function appendFiles(data, files, aOrB) {
-  let num = 1;
-  files.forEach(file => {
-    data.append(`${aOrB}${num}`, file);
-    num++;
-  });
 }
 
 function readFiles(files) {
